3589415b249080e9bb1f7b17a4f2bddaab5bf510
Philip Peterson <philip.c.peterson@gmail.com>
2014-10-03 00:03:16 -0400

    Change the app listing to make it more minimal

--- public/modules/gradApplications/config/gradApplications.client.config.js
+++ public/modules/gradApplications/config/gradApplications.client.config.js
@@ -4,8 +4,8 @@
 angular.module('gradApplications').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'GradApplications', 'gradApplications', 'dropdown', '/gradApplications(/create)?');
-		Menus.addSubMenuItem('topbar', 'gradApplications', 'List GradApplications', 'gradApplications');
-		Menus.addSubMenuItem('topbar', 'gradApplications', 'New GradApplication', 'gradApplications/create');
+		Menus.addMenuItem('topbar', 'Graduate Applications', 'gradApplications', 'dropdown', '/gradApplications(/create)?');
+		Menus.addSubMenuItem('topbar', 'gradApplications', 'List Graduate Applications', 'gradApplications');
+		Menus.addSubMenuItem('topbar', 'gradApplications', 'New Graduate Application', 'gradApplications/create');
 	}
 ]);
