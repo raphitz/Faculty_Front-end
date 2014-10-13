a840e9ca8a5abd577c98f1452a6ae4a5c6e67a6d
Philip Peterson <philip.c.peterson@gmail.com>
2014-10-13 06:48:17 -0400

    Fix production settings for Heroku

--- config/env/production.js
+++ config/env/production.js
@@ -17,7 +17,9 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/pdfmake/build/pdfmake.min.js',
+				'public/lib/pdfmake/build/vfs_fonts.js'
 			]
 		},
 		css: [ 'public/dist/application.min.css' ],
