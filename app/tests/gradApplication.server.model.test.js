57257a5f9723cc597079a70c83c3d0fe8b0e5696
Philip Peterson <philip.c.peterson@gmail.com>
2014-10-15 06:30:49 -0400

    Cleaned up tests

--- app/tests/gradApplication.server.model.test.js
+++ app/tests/gradApplication.server.model.test.js
@@ -11,7 +11,7 @@ var should = require('should'),
 /**
  * Globals
  */
-var user, gradApplication;
+var user, gradApplication, gradApplicationDup;
 
 /**
  * Unit tests
@@ -44,8 +44,7 @@ describe('GradApplication Model Unit Tests:', function() {
          });
       });
 
-      //id is not of proper length
-      it('should not be able to save an application with an invalid id: 123-123', function(done) { 
+      it('should not be able to save an application with a malformed UFID', function(done) { 
          gradApplication.personal_info.UFID = '123-123';
 
          return gradApplication.save(function(err) {
@@ -54,7 +53,15 @@ describe('GradApplication Model Unit Tests:', function() {
          });
       });
 
-      //id is alphanumeric
+      it('should not be able to save an application with a UFID containing illegal characters', function(done) { 
+         gradApplication.personal_info.UFID = '123A-1234';
+
+         return gradApplication.save(function(err) {
+            should.exist(err);
+            done();
+         });
+      });
+
       it('should not be able to save an application with an invalid id: 123A-1234', function(done) { 
          gradApplication.personal_info.UFID = '123A-1234';
 
@@ -64,33 +71,25 @@ describe('GradApplication Model Unit Tests:', function() {
          });
       });
 
-      it('should not be able to save more than one application for one student: id:1234-1234', function(done) { 
-            var gradApplicationDup = new GradApplication({
-               name: 'GradApplication Name2',
-               user: user
-            });
+      it('should not be able to save more than one application with same UFID', function(done) { 
 
             gradApplication.personal_info.UFID = '1234-1234';
             gradApplication.save(function(err) {
-            should.not.exist(err);
-            gradApplicationDup.personal_info.UFID = '1234-1234';
-            return gradApplicationDup.save(function(err) { //tries to save an application with dup UFID
-               should.exist(err);
-               done();
-            });
+               gradApplicationDup = new GradApplication({
+                  name: 'GradApplication Name2',
+                  user: user
+               });
+
+               should.not.exist(err);
+
+               gradApplicationDup.personal_info.UFID = '1234-1234';
+               return gradApplicationDup.save(function(err) {
+                  should.exist(err);
+                  done();
+               });
             });
       });
 
-      /*it('should be able to show an error when try to save with empty name', function(done) { 
-         //gradApplication.name = '';
-         gradApplication.personal_info.name.firstName = '';
-         gradApplication.personal_info.name.lastName = '';
-
-         return gradApplication.save(function(err) {
-            should.exist(err);
-            done();
-         });
-      });*/
    });
 
    afterEach(function(done) { 
