6a71d848bd8ec2a235d950414293b906388f8b45
John Reed <JohnReedLOL@ufl.edu>
2014-10-20 20:27:55 -0400

    Created Tabs in Applicant View

--- public/modules/gradApplications/controllers/gradApplications.client.controller.js
+++ public/modules/gradApplications/controllers/gradApplications.client.controller.js
@@ -103,6 +103,28 @@
    // GradApplications controller
    angular.module('gradApplications').controller('GradApplicationsController', ['$scope', '$stateParams', '$location', 'Authentication', 'GradApplications',
       function($scope, $stateParams, $location, Authentication, GradApplications ) {
+
+
+         //Tabs
+
+      
+        $scope.tabs = [
+          { title:'Dynamic Title 1', content:'Dynamic content 1' },
+          { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+        ];
+
+        $scope.alertMe = function() {
+          setTimeout(function() {
+            alert('You\'ve selected the alert tab!');
+          });
+        };
+      
+
+         //End of tabs functions
+
+
+
+         
          $scope.getprop = getprop;
 
          $scope.authentication = Authentication;
